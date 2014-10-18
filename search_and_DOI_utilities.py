c6c7e3a9f55d314f3acae9fb84a47baf2e68f908
Chris Holdgraf <choldgraf@berkeley.edu>
2014-10-18 16:30:34 -0700

    Updated the plos_search function to be a bit more concise/clear, and to perform more complicated searches

--- search_and_DOI_utilities.py
+++ search_and_DOI_utilities.py
@@ -14,65 +14,101 @@ from urllib2 import quote
 import json
 import re
 import codecs
+from copy import copy
 
-def plos_search(query, query_type = None, rows = 20, more_parameters = None, fq = '''doc_type:full AND article_type:"Research Article"''', output = "json", verbose = False):
+
+def plos_search(query='*', query_parameters=None, extra_parameters=None, rows=20, fq='''doc_type:full AND article_type:"Research Article"''',
+                output="json", verbose=False, api_key='...'):
     '''
     Accesses the PLOS search API.
-    query: the text of your query.
-    query_type: subject, author, etc.
-    rows: maximum number of results to return.
-    more_parameters: an optional dictionary; key-value pairs are parameter names and values for the search api.
-    fq: determines what kind of results are returned.
-    Set by default to return only full documents that are research articles (almost always what you want).
-    output: determines output type. Set to JSON by default, XML is also possible, along with a few others.
+
+    INPUTS
+    --------
+    query : str
+        A keyword to search for. This can be '*' if you want to return all results and search w/ query_parameters
+    query_parameters : (dict)
+        A dictionary of 'query_type: query' pairs. E.g.: {'author': 'Eisen'}
+        NOTE: Currently, it seems that 'author' is the only keyword you can use.
+        For all other keywords, use 'extra_parameters'
+    extra_parameters : (dict)
+        Extra parameters to pass. key-value pairs are parameter names and values for the search api.
+    rows : (int)
+        maximum number of results to return.
+    fq : (dict)
+        Determines what kind of results are returned.
+        Set by default to return only full documents that are research articles (almost always what you want).
+
+    output: (string)
+        determines output type. Set to JSON by default,
+        XML is also possible, along with a few others.
+
+    OUTPUT
+    --------
+    doc_json : (json)
+        A list of query results. Each item is a json entry representing an article.
     '''
-    api_key = "..."
+    PLOS_SEARCH_URL = "http://api.plos.org/search"
 
-    query_string = ""
-    if query_type:
-        query_string += query_type + ":"
-    query_string += '"' + query + '"'
+    # Create dictionaries we'll use in the query
+    if isinstance(query_parameters, dict):
+        query_strings = ["{0}:'{1}'".format(key, value)
+                         for key, value in query_parameters.iteritems()]
+    else:
+        query_strings = []
+    query_dict = {'q': ' AND '.join(query_strings + [query])}
+    query_dict.update({'fq': fq,
+                       'wt': output,
+                       'rows': str(rows),
+                       'api_key': api_key})
 
-    params_string = ""
-    if more_parameters:
-        params_string = "&" + "&".join([key + "=" + quote(value) for key, value in more_parameters.iteritems()])
+    if extra_parameters is not None:
+        query_dict.update(extra_parameters)
 
-    fq_string = "&fq=" + quote(fq)
-
-    url = "http://api.plos.org/search?q=" + query_string + params_string + fq_string + "&wt=" + output + "&rows=" + str(rows) + "&api_key=" + api_key
     headers = {'Content-Type': 'application/' + output}
+
+    # Build the final query and post a GET request
+    r = requests.get(PLOS_SEARCH_URL, params=query_dict, headers=headers)
+    r.encoding = "UTF-8"  # just to be sure
+
+    doc_json = r.json()["response"]["docs"]
     if verbose:
-        print url
-    r = requests.get(url, headers=headers)
-    r.encoding = "UTF-8" # just to be sure
-    return r.json()["response"]["docs"]
+        print query_dict['q']
+        print r.url
+        if len(doc_json) == 0:
+            print('No results found')
+    return doc_json
+
 
 def plos_dois(search_results):
     '''Turns search results from plos_search into a list of DOIs.'''
     return [paper["id"] for paper in search_results]
 
-def remote_XML_retrieval(doi, destination = None):
+
+def remote_XML_retrieval(doi, destination=None):
     '''
     Given the DOI of a PLOS paper, downloads the XML and parses it using Beautiful Soup.
     If you'd like to save the XML as a file, set destination to a filename.
     '''
-    headers = {"Content-Type":"application/xml"}
-    r = requests.get("http://www.plosone.org/article/fetchObjectAttachment.action?uri=info:doi/" + doi + "&representation=XML")
+    DOI_URL = "http://www.plosone.org/article/fetchObjectAttachment.action?uri=info:doi/"
+    headers = {"Content-Type": "application/xml"}
+    r = requests.get(DOI_URL + doi + "&representation=XML")
     # Doesn't matter whether it's a PLOS ONE article or not -- this will work for any article in any PLOS journal.
-    r.encoding = "UTF-8" # This is needed to keep the encoding on the papers correct.
+    r.encoding = "UTF-8"  # This is needed to keep the encoding on the papers correct.
     if destination:
-        f = codecs.open(destination, "w", "utf-8")
-        f.write(r.text)
-        f.close()
-    soup = BeautifulSoup(r.text, features = "xml")
+        with codecs.open(destination, "w", "utf-8") as f:
+            f.write(r.text)
+    soup = BeautifulSoup(r.text, features="xml")
     return soup
+
 
 def local_XML_parsing(filename):
     '''Opens the given XML file, parses it using Beautiful Soup, and returns the output.'''
+    
     f = codecs.open(filename, "r", "utf-8")
     soup = BeautifulSoup(f, features = "xml")
     f.close()
     return soup
+
 
 def paper_doi(paper):
     '''Given a soupified PLOS XML paper, returns that paper's DOI.'''
